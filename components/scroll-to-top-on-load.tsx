b6b5c5b41a9fe2eded2e5a37308d6b4ac702aeb0
Mohammad Ninad Mahmud Nobo <87571074+mninadmnobo@users.noreply.github.com>
2026-05-25 05:27:54 +0600

    scrollable

--- components/scroll-to-top-on-load.tsx
+++ components/scroll-to-top-on-load.tsx
@@ -5,10 +5,8 @@ import { useEffect } from "react"
 export function ScrollToTopOnLoad() {
   useEffect(() => {
     if ("scrollRestoration" in window.history) {
-      window.history.scrollRestoration = "manual"
+      window.history.scrollRestoration = "auto"
     }
-
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" })
   }, [])
 
   return null
