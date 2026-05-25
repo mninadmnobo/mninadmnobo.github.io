b2f1a8ae466beffbb6b8aa765ecb053383017c79
Mohammad Ninad Mahmud Nobo <87571074+mninadmnobo@users.noreply.github.com>
2026-05-25 06:10:02 +0600

    scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Projects } from "@/components/projects"
 import { Research } from "@/components/research"
 import { TechStack } from "@/components/tech-stack"
 import { Contact } from "@/components/contact"
+import { ProfessionalForm } from "@/components/professional-form"
 import { BackToTop } from "@/components/back-to-top"
 import { Footer } from "@/components/footer"
 
@@ -18,6 +19,9 @@ export default function Home() {
       <Research />
       <TechStack />
       <Contact />
+      <div className="flex justify-center px-6 md:px-12 lg:px-24">
+        <ProfessionalForm />
+      </div>
       <Footer />
       <BackToTop />
     </main>
